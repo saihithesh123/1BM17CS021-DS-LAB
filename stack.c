fe7eb49f16edbcb579c844bd0968e0e19cacf5be
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2019-09-04 08:20:10 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int s[100])
+int push(int ele,int s[100],int*t)
+void main()
+{
+int stack[100],top=-1,int x;
+int push (1,stack,&top);
+itn push(2,stack,&top);
+x=pop(stack,top);
+if(x!=999)
+{
+printf("popped element is %d \n",x);
+display(stack,top);
+return 0;
+}
+void push[int ele,int s[100],int*t)
+{
+if (*y==99)
+{
+printf("stack is overflow \n");
+return;
+}
+*t=*t*1;
+s[*t]=ele;
+}
+display(stack,top);
+void display(int s[100],int t)
+{
+int i;
+if(t==1)
+{
+printf("stack is only");
+return;
+}
+for(i=t;i>=0;i--);
+printf("%d \n" ,s[i]);
+}
+int pop(int s[100],int*t)
+{
+if(*t==-1)
+printf("stack underflow \n");
+return ==9999;
+}
+}
