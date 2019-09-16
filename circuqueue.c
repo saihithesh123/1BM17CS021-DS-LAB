ad25a5b86a8c198906f4601f874f55a3cd06f6c6
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2019-09-16 20:07:43 +0530

    Update circuqueue.c

--- circuqueue.c
+++ circuqueue.c
@@ -1,98 +1,85 @@
-#include <stdio.h>
-#define size 5
-
-void insertq(int[], int);
-void deleteq(int[]);
-void display(int[]);
-
-int front =  - 1;
-int rear =  - 1;
-
+#include<stdio.h>
+#define SIZE 10
+void insert(int q[],int *r,int *f,int item)
+{
+ if((*f==*r+1)||(*f=0 && *r==SIZE-1))
+ {
+  printf("Queue Overflow\n");
+  return;
+ }
+ if(*f==-1)
+ *r=(*r+1)%SIZE;
+ q[*r]=item;
+}
+int delete(int q[],int *r,int *f)
+{
+ int item;
+ if(*f==-1)
+ {
+  printf("Queue Underflow\n");
+  return -999;
+ }
+ else
+  item=q[*f];
+ if(*f==*r)
+ {
+  *f=-1;
+  *r=-1;
+ }
+ else 
+  *f=(*f+1)%SIZE;
+ return item;
+}
+void display(int q[],int r,int f)
+{
+ int i;
+ if(f>r)
+ {
+  printf("Queue is empty\n");
+  return;
+ }
+ printf("Contents of the queue are:\n");
+ for(i=f;i<=r;i=(i+1)%SIZE)
+  printf("%d ",q[i]);
+}
 int main()
 {
-int n, ch;
-int queue[size];
-do
-{
- printf("\n\n Circular Queue:\n1. Insert \n2. Delete\n3. Display\n0. Exit");
- printf("\nEnter Choice 0-3? : ");
- scanf("%d", &ch);
- switch (ch)
-{
-  case 1:
-  printf("\nEnter number: ");
-   scanf("%d", &n);
-   insertq(queue, n);
-    break;
-    case 2:
-                deleteq(queue);
-                break;
-            case 3:
-                display(queue);
-                break;
-        }
-    }while (ch != 0);
-}
+ int f=-1,r=-1;
+ int q[SIZE];
+ int op,sel;
+ do
+ {
+  printf("Choose an operation to perform:\n");
+  printf("1.Insert\n");
+  printf("2.Delete\n");
+  printf("3.Display\n");
+  scanf("%d",&op);
 
-
-void insertq(int queue[], int item)
-{
-    if ((front == 0 && rear == size - 1) || (front == rear + 1))
-    {
-        printf("queue is full");
-        return;
-    }
-    else if (rear ==  - 1)
-    {
-        rear++;
-        front++;
-    }
-    else if (rear == size - 1 && front > 0)
-    {
-        rear = 0;
-    }
-    else
-    {
-        rear++;
-    }
-    queue[rear] = item;
-}
-
-void display(int queue[])
-{
-    int i;
-    printf("\n");
-    if (front > rear)
-    {
-        for (i = front; i < size; i++)
-        {
-            printf("%d ", queue[i]);
-        }
-        for (i = 0; i <= rear; i++)
-            printf("%d ", queue[i]);
-    }
-    else
-    {
-        for (i = front; i <= rear; i++)
-            printf("%d ", queue[i]);
-    }
-}
-
-void deleteq(int queue[])
-{
-    if (front ==  - 1)
-    {
-        printf("Queue is empty ");
-    }
-else if (front == rear)
-{
-   printf("\n %d deleted", queue[front]);
-   front =  - 1;
-   rear =  - 1;
-}
-else
-{
-    printf("\n %d deleted", queue[front]);
-    front++;
-}
-}
+  switch(op)
+  {
+   case 1:{
+	   int item;
+	   printf("Enter the element to be inserted\n");
+	   scanf("%d",&item);
+	   insert(q,&r,&f,item);
+	   break;
+	  }
+   case 2:{
+	   int x;
+	   x=delete(q,&r,&f);
+	   if(x==-999)
+	     break;
+	   printf("Deleted element is %d\n",x);
+	   break;
+	  }
+   case 3:{
+	   display(q,r,f);
+	   break;
+	  }
+   default: printf("Invalid option\n");
+  }
+  printf("\nDo you want to continue?(press 1)\n");
+  scanf("%d",&sel);
+ } while(sel==1);
+ return 1;
+}     
